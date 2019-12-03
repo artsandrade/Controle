ddc9979bfaa444264d138c91ed161955c9e66344
Arthur Andrade <arthursandrade@icloud.com>
2019-12-03 18:02:31 -0300

    6ºCommit - Ajustando painel django-admin

--- controle/estoque/admin.py
+++ controle/estoque/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import Estoque, EstoqueItens
 
+
+
+class EstoqueItensInLine(admin.TabularInline):
+    model = EstoqueItens
+    extra = 0
+
 @admin.register(Estoque)
 class EstoqueAdmin(admin.ModelAdmin):
+    inlines = (EstoqueItensInLine,)
     list_display = ('__str__', 'nf')
     search_fields=('nf',)
     list_filter=('funcionario',)
